0ead480c51ae03298086533258affabb7a7877c2
muhammadMusa666 <Unkox1890@gmail.com>
2025-11-28 21:24:02 +0500

    Made With Love By Musa!

---
+++ Challenge #1/script.js
@@ -0,0 +1,63 @@
+// Challenging Yourself:
+// I Usually Dont Do This, But Today, I Have Made This Subrepo,
+// Just For A Challenge, So You Test And Challenge Yourself,
+// Because, You Cant Just Keep Watching And Reading This Without Being Tested,
+// So, In This Subrepo, The Challenge Will Be, That You Implement,
+// The "Divide And Conquer" Technique, Which,
+// I Dont Remember If I've Told You Before, But Basically,
+// You Divide The Main Problem Into Smaller Sub-Problems,
+// And Then Solve Each Sub-Problem Individually,
+// Because The Sub-Problems Are Easier To Solve Obviously,
+// So You First Identify The Main Problem,
+// Ask Questions To Yourself Of What To Do And How,
+// So You First Understand The Main Problem, Then Divide It Into Sub-Problems,
+// And So The Main Problem Will Be Solved, And Even If You Can't Solve It,
+// Perfectly Or Just Werent Able To, Then Its Alright😄! Because,
+// I Just Want To Make You Use All Of Your Mind, Skills And Knowledge That You Have,
+// So If You Test This Method Out, Its Fine Even If You Fail,
+// And Dont Even THINK About Worrying If You Didnt Do It Perfectly😅,
+// Because You Didnt Fail, And Doind It Your Own Way?
+// Thats Way Better Than Even Doing It Perfectly!,
+// Because Your Finding And Developing Your Own Coding Style😁,
+// So, I Hope You Atleast Tried To Solve It, And Didnt Just Skip It,
+// Anyways, Here's Challenge:
+
+/* Given An Array Of Forecasted Maximum Temperatures,
+The Thermometer Displays A String With These Temperatures,
+Example: [17, 21, 23] Will Print "... 17°C In 1 Days... 21°C In 2 Days... 23°C In 3 Days..."
+Your Tasks:
+1. Create A Function 'printForecast' Which Takes In An Array 'arr',
+And Logs A String Like The Above To The Console.
+Try It With Both Test Data Sets,
+
+2. Use The Problem-Solving Framework:
+Understand The Problem, And Break It Up Into Sub-Problems!
+
+Test Data:
+Data 1: [17, 21, 23]
+Data 2: [12, 5, -5, 0, 4]
+*/
+
+// So You Can Solve It In The Large Empty Space Below,
+// Or Just Create A Separate JS File For It,
+// But I Am Saying This Again, At Least Try To Solve It Yourself,
+// Now, If You Want To Find The Answer, Then Just Go To The answer.js File,
+// So Finally, Good Luck, And All The Best, Even If You Failed But Tried😅!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
