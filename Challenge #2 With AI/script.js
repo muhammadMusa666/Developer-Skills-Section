791b403079ea04415cdd5308a1340d414b531007
muhammadMusa666 <Unkox1890@gmail.com>
2025-12-01 21:32:17 +0500

    Made With Love By Musa!

---
+++ Challenge #2 With AI/script.js
@@ -0,0 +1,63 @@
+// Challenge #2 With AI:
+// This Is The 2nd Challenge, And Surprisingly, This Is The Last Subrepo,
+// Yes! So In This 2nd Challenge, We Will Use AI,
+// Now I Made Another Challenge Before This, And I Kind Of,
+// Dont Make Challenge Subrepos, So I'll Keep This 2nd Challenge Short,
+// And By The Way, You Can Use Any AI You Like, I Will Be Using Chatgpt,
+// Or Kilo Code, So, Lets Begin With The Challenge!
+// Note: You Must Use AI's For This Challenge, Because This Is Revolved Around It,
+// And It Uses JavaScript Features We Havent Learned About Yet,
+// So You Can Try To Solve It Yourself, But Chances Are You Wont,
+
+// So Here's The Challenge:
+// Lets Say Your Building A Time-Tracking Application,
+// Built For Freelancers, And At Some Point, You'll Need A function,
+// That Receives, Daily Work Hours For A Certain Week,
+// And Then It Will Return 5 Data Points, So:
+// 1: Total Hours Worked,
+// 2: Average Daily Hours,
+// 3: The Day With The Most Hours Worked,
+// 4: Number Of Days Worked,
+// 5: Wether The Week Was Full-Time (Worked 35 Hours Or More),
+
+// Challenge Data:[7.5, 8, 6.5, 0, 8.5, 4, 0];
+
+// Now You Can Solve All Of This With AI,
+// But There Is A High Chance For Bugs Or Other Stuff,
+// So Always Check Your Code If You Copied It From An AI,
+// Its Less Likely If You Use A Coding AI Like Kil Code,
+// Now You Can Solve This In The Large Area Below, Or In Another File,
+// And If You Want To Find The Answer, Then Just Go To The answer.js File,
+// So, Good Luck!
+
+// (Side-Note: You Can Use The Same AI, To Explain The Stuff In The AI's Given Code,
+// Because There Is A High Chance That You Dont Understand Some Parts Of The Code,
+// So I Recommend You Doing So, Now You Can Continue The Challenge😅)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
